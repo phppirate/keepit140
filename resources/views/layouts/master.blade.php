adbafdcb93b22d433b14a0f92d7372736ccdf73e
Matt Stauffer <matt@tighten.co>
2017-11-10 07:11:58 -0500

    wip

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Keep It 140</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="">
+            @yield('content')
+        </div>
+    </body>
+</html>
